39cedbc93f1c41a2f2204d7edba00cbe1d6e9447
destefano1986 <destefano@sina.cn>
2018-07-27 15:34:07 +0800

    Modify SQL 20180727

--- Temp.py
+++ Temp.py
@@ -19,7 +19,7 @@ cursor = conn.cursor()
 #sql = "SELECT datetime ,userlabel, rrutotalprbusagemeanul, rrutotalprbusagemeandl, rrcsuccconnestab, rrcattconnestab, erabnbrsuccestab, erabnbrattestab FROM pm_eutrancelltdd_hour WHERE datetime = '2018-06-14 03:00:00'"
 #sql = "SELECT datetime ,userlabel, pdcpupoctul, pdcpupoctdl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-22 00:00:00'"
 #sql = "SELECT datetime ,userlabel, pdcpupoctul, pdcpupoctdl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-20 00:00:00'"
-#sql = "SELECT dn, userlabel, pdcpupoctul, pdcpupoctdl, rrutotalprbusagemeanul, rrutotalprbusagemeandl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-17 00:00:00'"
+#sql = "SELECT dn, userlabel, pdcpupoctul, pdcpupoctdl, rrutotalprbusagemeanul, rrutotalprbusagemeandl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-01 00:00:00'"
 #sql = "SELECT datetime, avg(rrutotalprbusagemeanul) AS avg_rrutotalprbusagemeanul, avg(rrutotalprbusagemeandl) AS avg_rrutotalprbusagemeanul, avg(pdcpupoctul) AS avg_pdcpupoctul, avg(pdcpupoctdl) AS avg_pdcpupoctdl, avg(rrcconnmax) AS avg_rrcconnmax, avg(rrcconnmean) AS avg_rrcconnmean FROM pm_eutrancelltdd_day WHERE datetime BETWEEN '2018-07-01 00:00:00' AND '2018-07-18 00:00:00' GROUP BY datetime"
 #sql = "SELECT * FROM resource_eutrancell WHERE date = '2018-07-25'"
 #sql = "SELECT n.dn,n.A2ThresholdInterFcov,n.A4ThresholdInterFcov,r.* FROM (SELECT dn,A2ThresholdInterFcov,A4ThresholdInterFcov FROM nrm_eutrancelltdd WHERE date = '2018-07-09') as n,(select * FROM resource_eutrancell WHERE date = '2018-07-09') as r WHERE n.dn = r.dn"
@@ -28,12 +28,13 @@ cursor = conn.cursor()
 #sql = "SELECT * FROM irms_wireless_antenna WHERE date = '2018-06-26'"
 #sql = "SELECT * FROM irms_wireless_antenna WHERE date = '2018-06-27'"
 #sql = "SELECT dn, userlabel, qrxlevmin, referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-25'"
-sql = "SELECT n.dn,n.referencesignalpower,r.* FROM (SELECT dn,referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-26') as n,(select * FROM resource_eutrancell WHERE date = '2018-07-26') as r WHERE n.dn = r.dn"
+#sql = "SELECT n.dn,n.referencesignalpower,r.* FROM (SELECT dn,referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-26') as n,(select * FROM resource_eutrancell WHERE date = '2018-07-26') as r WHERE n.dn = r.dn"
+sql = "SELECT dn, userlabel, pdcpupoctul, pdcpupoctdl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-26 00:00:00'"
 cursor.execute(sql)
 title = [i[0] for i in cursor.description]
 data = cursor.fetchall()
 df = pd.DataFrame(data, columns = title)
-df.to_csv('nrm_eutrancelltdd_resource_eutrancell_0725.csv', index=True, header=True, encoding='gbk')
+df.to_csv('pm_eutrancelltdd_day_0726.csv', index=True, header=True, encoding='gbk')
 #print (data)
 cursor.close()
 conn.close()
