5247f677681a36b65784f612d4428efe4a0f5201
destefano1986 <destefano@sina.cn>
2018-07-28 09:31:20 +0800

    Modify SQL 20180728

--- Temp.py
+++ Temp.py
@@ -23,18 +23,18 @@ cursor = conn.cursor()
 #sql = "SELECT datetime, avg(rrutotalprbusagemeanul) AS avg_rrutotalprbusagemeanul, avg(rrutotalprbusagemeandl) AS avg_rrutotalprbusagemeanul, avg(pdcpupoctul) AS avg_pdcpupoctul, avg(pdcpupoctdl) AS avg_pdcpupoctdl, avg(rrcconnmax) AS avg_rrcconnmax, avg(rrcconnmean) AS avg_rrcconnmean FROM pm_eutrancelltdd_day WHERE datetime BETWEEN '2018-07-01 00:00:00' AND '2018-07-18 00:00:00' GROUP BY datetime"
 #sql = "SELECT * FROM resource_eutrancell WHERE date = '2018-07-25'"
 #sql = "SELECT n.dn,n.A2ThresholdInterFcov,n.A4ThresholdInterFcov,r.* FROM (SELECT dn,A2ThresholdInterFcov,A4ThresholdInterFcov FROM nrm_eutrancelltdd WHERE date = '2018-07-09') as n,(select * FROM resource_eutrancell WHERE date = '2018-07-09') as r WHERE n.dn = r.dn"
-#sql = "SELECT date, dn, userlabel, A2ThresholdInterFcov, A4ThresholdInterFcov FROM nrm_eutrancelltdd WHERE date = '2018-04-09'"
+sql = "SELECT date, dn, userlabel, referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-26'"
 #sql = "SELECT * FROM irms_wireless_antenna WHERE date = '2018-06-25'"
 #sql = "SELECT * FROM irms_wireless_antenna WHERE date = '2018-06-26'"
 #sql = "SELECT * FROM irms_wireless_antenna WHERE date = '2018-06-27'"
 #sql = "SELECT dn, userlabel, qrxlevmin, referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-25'"
 #sql = "SELECT n.dn,n.referencesignalpower,r.* FROM (SELECT dn,referencesignalpower FROM nrm_eutrancelltdd WHERE date = '2018-07-26') as n,(select * FROM resource_eutrancell WHERE date = '2018-07-26') as r WHERE n.dn = r.dn"
-sql = "SELECT dn, userlabel, pdcpupoctul, pdcpupoctdl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-26 00:00:00'"
+#sql = "SELECT n.dn, n.userlabel, n.pdcpupoctul, n.pdcpupoctdl, r.* FROM (SELECT dn, userlabel, pdcpupoctul, pdcpupoctdl FROM pm_eutrancelltdd_day WHERE datetime = '2018-07-26 00:00:00') AS n, (SELECT * FROM resource_eutrancell WHERE date = '2018-07-26') as r WHERE n.dn=r.dn"
 cursor.execute(sql)
 title = [i[0] for i in cursor.description]
 data = cursor.fetchall()
 df = pd.DataFrame(data, columns = title)
-df.to_csv('pm_eutrancelltdd_day_0726.csv', index=True, header=True, encoding='gbk')
+df.to_csv('pm_eutrancelltdd_day_0726_2.csv', index=True, header=True, encoding='gbk')
 #print (data)
 cursor.close()
 conn.close()
